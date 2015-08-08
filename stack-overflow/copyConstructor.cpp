be80bb95b2d3e8c9e97985b957edca379e211e68
Kevin Yuan <kevinyuan555@gmail.com>
2015-08-08 16:49:46 -0400

    copy constructor test

--- stack-overflow/copyConstructor.cpp
+++ stack-overflow/copyConstructor.cpp
@@ -1,2 +1,44 @@
 #include <iostream>
 
+using namespace std;
+
+class Base 
+{
+  public:
+  int v;
+  Base(int val=0);
+  Base(const Base &obj);
+};
+
+Base::Base(int val)
+  : v(val)
+{
+  cout << "Constructor\n";
+}
+
+Base::Base(const Base &obj)
+  : v(obj.v)
+{
+  cout << "Copy Constructor\n";
+}
+
+Base IncrementBaseValue(Base& b)
+{
+  b.v++;
+  return b;
+}
+
+int main ()
+{
+  Base b(1);
+
+  Base e = IncrementBaseValue(b);
+  b.v = 3;
+
+  cout << b.v << endl;
+  cout << e.v << endl;
+
+  return 0;
+}
+
+
