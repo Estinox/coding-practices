059570a66bd56d523f1d668a3de63944788d8260
Kevin Yuan <kevinyuan555@gmail.com>
2015-08-28 10:17:53 -0400

    mushroom_monster completed

--- code-jam/2015_mushroom_monster/mushroom_monster.cc
+++ code-jam/2015_mushroom_monster/mushroom_monster.cc
@@ -13,27 +13,29 @@ int main()
     assert (i >= 0);
 
     int N = 0, one = 0, two = 0;
-    int l[N];
-    scanf("%d %d", &N, &l[0]);
+    scanf("%d", &N);
 
-    printf("first: %d", l[0]);
+    int l[N];
+    scanf("%d", &l[0]);
+
     for (int j = 1; j < N; ++j)
     {
       scanf("%d", &l[j]);
-      one += l[j-1] - l[j];
+      one += std::max(0, l[j-1] - l[j]);
     }
 
     // Solve for two
     int max_rate = l[N-2] - l[N-1];
-    for (int n = 0; n < N-1; ++n)
+    //printf("max_rate: %d, %d - %d\n", max_rate, l[N-2], l[N-1]);
+    for (int n = 0; n < N-1; n++)
     {
+      //printf("two: %d, l[%d] %d\n", two, n, l[n]);
       two += std::min(max_rate, l[n]);
     }
 
-    printf("\n");
-
-    printf("Case #%d: %d %d", i+1, one, two);
+    printf("Case #%d: %d %d\n", i+1, one, two);
   }
+
 
   return 0;
 }
