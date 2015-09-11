ced296ba5dba38ffefa0c09a72d5df2bba2b754c
Kevin Yuan <kevinyuan555@gmail.com>
2015-09-11 16:22:25 -0400

    templates

--- geeks-for-geeks/linked_list_main.cc
+++ geeks-for-geeks/linked_list_main.cc
@@ -3,14 +3,38 @@
 
 int main()
 {
-  Node *head = nullptr; 
+  Stack<int> intStack;  // stack of ints
+  Stack<string> stringStack;    // stack of strings
+
+  // manipulate int stack
+  intStack.push(7);
+  cout << intStack.top() << endl;
+
+  // manipulate string stack
+  stringStack.push("hello");
+  cout << stringStack.top() << std::endl;
+  stringStack.pop();
+  stringStack.pop();
+
+  /*
+  Node<int> *tmp = new Node<int>(5);
+
+  //Node<int> *head = nullptr;
+  //Node<int> temp = Node(10);
+
+  //Push(&head, 0);
+
   for (int i = 5; i >= 0; --i)
   {
     Push(&head, i);
   }
-
-  ReverseLinkedList(&head);
   head->PrintList();
+  */
+
+  //DeleteNodeIterative(&head, 0);
+
+  //head->PrintList();
 
   return 0;
 }
+
